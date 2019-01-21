70a388e5109be9cf8c6e96cb025cba9debba8961
Daniell Zheng <daniellzheng440@gmail.com>
2019-01-20 23:21:12 -0500

    add more to run on Heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/waste-info'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/waste-info/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
